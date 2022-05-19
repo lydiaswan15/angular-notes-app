29564fb87098ea34aa2f2baad69f1210e512e0b9
lydiaswan15 <lydiaswan15@gmail.com>
2022-05-19 14:33:38 -0600

    W05 complete.

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,36 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+@Injectable()
+export class RecipeService{
+// Place where we manage our recipes
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('Snitzel', 'Yum', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', 
+        [
+            new Ingredient('Meat', 1), 
+            new Ingredient(
+                "French Fries", 1
+            )
+        ]),
+        new Recipe('Burger', 'yum', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', 
+        [
+            new Ingredient('Buns', 2), 
+            new Ingredient('Meat', 1)
+        ])
+    ];
+
+    constructor(private slService: ShoppingListService){}
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
+    }
+
+
+}
